51bd441e3f26668c2b9bff69ce1a5a282abcb309
Ohto-Ai <zhu.thatboy@outlook.com>
2024-08-30 11:58:44 +0800

    Change port

--- main.py
+++ main.py
@@ -52,4 +52,4 @@ def static_file(filename):
     return send_from_directory(STATIC_FOLDER, filename)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8050)
